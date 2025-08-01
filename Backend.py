94c06d20a24da55a286679a01544f6e0521837ed
Gladturtle <akashsam88@gmail.com>
2025-08-01 15:21:03 +0530

    HGelo

--- Backend.py
+++ Backend.py
@@ -1,25 +1,36 @@
-import requests
-class Backend():
-    
-    def __init__(self):
-        self.productID = 0
-        self.lat = 0
-        self.long =0
-        self.apikey = 'Bearer V64BVVPQM9431DHQYVC8P5WEM4'
-    def call_seaapi(self):
-        
-        resp = requests.post('https://api.climatiq.io/freight/v2/intermodal',headers={'Authorization': self.apikey},data = {
-        "emission_factor": {
-            "activity_id": "electricity-supply_grid-source_residual_mix",
-            "data_version": "^21"
-        },
-        "parameters":
-            {
-            "energy": 4200,
-            "energy_unit": "kWh"
-            }
-        })
-        print(resp.text)
+from flask import Flask, render_template,make_response,send_file,abort,request,redirect
+from flask_restful import Api,Resource,fields,reqparse,marshal_with,marshal
+import ollama
 
-b1 = Backend()
-b1.call_seaapi()        
+app = Flask(__name__)
+api = Api(app)
+app.config['SECRET_KEY'] = 'bacaa3381e4c453088449ccd04f270cf'
+
+add = '127.0.0.1:5000'
+
+loc_parser = reqparse.RequestParser()
+loc_parser.add_argument('location',type=str,required = True)
+
+class AkashBot:
+    def __init__(self):
+        ollama.pull(model='deepseek-r1:latest')
+        self.client = ollama.Client()
+        self.client.create(model='InfrastructureInsight',from_ = 'deepseek-r1:latest', messages=[{'role':'system','content':
+                                                                                            'You are a infrastructure insight specialist who looks at soil content and natural diasters in that particular area.You will only have the area name. You then advices builders on how to build their buildings. DO NOT INTRODUCE YOURSELF. Do not mention the user, only generate a professional report. No Comments'}])
+    def generate_response(self,loc):
+        response = self.client.chat(model = 'InfrastructureInsight',messages=[{'role':'system','content':
+                                                                                            'You are a infrastructure insight specialist who looks at soil content and natural diasters in that particular area.You will only have the area name. You then advices builders on how to build their buildings. DO NOT INTRODUCE YOURSELF. Do not mention the user, only generate a professional report. No Comments'},{'role':'user','content':'The location is '+loc}])
+        return response
+class Home(Resource):
+    def post(self):
+        args = loc_parser.parse_args()
+        loc = args['location']
+        print(loc)
+        a1 = AkashBot()
+        response = a1.generate_response(loc)
+        message = response['message']['content']
+        return {'advice':response['message']['content'][message.index('</think>')+10:]}
+
+api.add_resource(Home,'/ai/')
+
+app.run(debug=True)
